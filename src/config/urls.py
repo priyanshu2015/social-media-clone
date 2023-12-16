c9ffd3b6e9bf58ea58691775cb1cf49e30e147d8
unknown <priyanshu.gupta2008@gmail.com>
2023-12-16 18:55:45 +0530

    fixed static and media root urls

--- src/config/urls.py
+++ src/config/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from mainapp.views import HomeView
 from django.conf import settings
 from django.conf.urls.static import static
 from users.api.views import SetCSRFCookie
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -36,7 +37,7 @@ if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 else:
     urlpatterns += [
-        static(settings.STATIC_URL, document_root=settings.STATIC_ROOT),
-        static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+        re_path(r"^static/(?P<path>.*)$", serve, {"document_root": settings.STATIC_ROOT}),
+        re_path(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
     ]
 
